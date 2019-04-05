0b88cb2816a0c4f7994d4259a275fc08621af165
Joe Reid <joe@joereid.com>
2019-04-05 19:31:46 -0400

    more whitespace

--- g_local.h
+++ g_local.h
@@ -379,17 +379,16 @@ typedef enum
 // edict->movetype values
 typedef enum
 {
-MOVETYPE_NONE,			// never moves
-MOVETYPE_NOCLIP,		// origin and angles change with no interaction
-MOVETYPE_PUSH,			// no clip to world, push on box contact
-MOVETYPE_STOP,			// no clip to world, stops on box contact
-
-MOVETYPE_WALK,			// gravity
-MOVETYPE_STEP,			// gravity, special edge handling
-MOVETYPE_FLY,
-MOVETYPE_TOSS,			// gravity
-MOVETYPE_FLYMISSILE,	// extra size to monsters
-MOVETYPE_BOUNCE
+	MOVETYPE_NONE,         // never moves
+	MOVETYPE_NOCLIP,       // origin and angles change with no interaction
+	MOVETYPE_PUSH,         // no clip to world, push on box contact
+	MOVETYPE_STOP,         // no clip to world, stops on box contact
+	MOVETYPE_WALK,         // gravity
+	MOVETYPE_STEP,         // gravity, special edge handling
+	MOVETYPE_FLY,
+	MOVETYPE_TOSS,         // gravity
+	MOVETYPE_FLYMISSILE,   // extra size to monsters
+	MOVETYPE_BOUNCE
 } movetype_t;
 
 
