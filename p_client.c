43b20dbb1e57e585f9a1223dcfaf03a0f3127de3
Joe Reid <joe@joereid.com>
2019-12-26 10:26:44 -0500

    #13 moved timer stuff from gclient_s to client_persistent_t to survive frag and respawns - forgot these files..

--- p_client.c
+++ p_client.c
@@ -1394,16 +1394,16 @@ void ClientBeginDeathmatch (edict_t *ent)
 	TDM_UpdateHud(ent, true);
 
 	// set timer icons indexes (armor/weapon is set on the fly)
-	client->item_timer_icon[TIMER_QUAD] = gi.imageindex ("p_quad");
-	client->item_timer_icon[TIMER_INVULN] = gi.imageindex ("p_invulnerability");
-	client->item_timer_icon[TIMER_ENVIROSUIT] = gi.imageindex ("p_envirosuit");
-	client->item_timer_icon[TIMER_REBREATHER] = gi.imageindex ("p_rebreather");
+	client->pers.item_timer_icon[TIMER_QUAD] = gi.imageindex ("p_quad");
+	client->pers.item_timer_icon[TIMER_INVULN] = gi.imageindex ("p_invulnerability");
+	client->pers.item_timer_icon[TIMER_ENVIROSUIT] = gi.imageindex ("p_envirosuit");
+	client->pers.item_timer_icon[TIMER_REBREATHER] = gi.imageindex ("p_rebreather");
 
-	client->timer1.stat_index = STAT_TIMER;
-	client->timer2.stat_index = STAT_TIMER2;
+	client->pers.timer1.stat_index = STAT_TIMER;
+	client->pers.timer2.stat_index = STAT_TIMER2;
 
 	// should update timers
-	client->next_timer_update = 0;
+	client->pers.next_timer_update = 0;
 
 	//no cross-level menus!
 	PMenu_Close (ent);
