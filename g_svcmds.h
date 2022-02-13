3204a5889d202efc9c8d22cbba553fc4acddecc2
Joe Reid <joe@joereid.com>
2022-02-13 18:14:56 -0500

    formatting

--- g_svcmds.h
+++ g_svcmds.h
@@ -1,17 +1,16 @@
-typedef struct
-{
-	unsigned	mask;
-	unsigned	compare;
-	int			expire;
+typedef struct {
+    unsigned    mask;
+    unsigned    compare;
+    int         expire;
 } ipfilter_t;
 
-#define	MAX_IPFILTERS	1024
+#define MAX_IPFILTERS   1024
 
-extern ipfilter_t	ipfilters[MAX_IPFILTERS];
-extern unsigned	numipfilters;
+extern ipfilter_t   ipfilters[MAX_IPFILTERS];
+extern unsigned     numipfilters;
 
-void SVCmd_AddIP_f (edict_t *ent, char *ip, int exp);
-void SVCmd_RemoveIP_f (edict_t *ent, char *ip);
-void SVCmd_ListIP_f (edict_t *ent);
-qboolean StringToFilter (const char *s, ipfilter_t *f, int exp);
-qboolean CheckIpFilterList (edict_t *ent, unsigned *i);
+void SVCmd_AddIP_f(edict_t *ent, char *ip, int exp);
+void SVCmd_RemoveIP_f(edict_t *ent, char *ip);
+void SVCmd_ListIP_f(edict_t *ent);
+qboolean StringToFilter(const char *s, ipfilter_t *f, int exp);
+qboolean CheckIpFilterList(edict_t *ent, unsigned *i);
