223f5b135b5ceadd6329506677586364cc85104d
Joe Reid <joe@joereid.com>
2023-08-01 10:46:01 -0500

    Revert "statusbars had garage at the end"
    This reverts commit 633b0af6111f4c11b13d0cf5e9b79d0d5f39d9fd.

--- g_local.h
+++ g_local.h
@@ -523,7 +523,6 @@ typedef struct
 	int           numspawns;
 	edict_t       *spawns[TDM_MAX_MAP_SPAWNPOINTS];
 	time_t        spawntime;
-	char          statusbar[2000];
 } level_locals_t;
 
 
@@ -1484,7 +1483,6 @@ typedef struct
 	int            armor_timer;           // auto timer for armor
 	int            armor_mask;            // armor to auto timer
 	int            timeout_count;         // how many timeouts did we call?
-	char           statusbar[2000];       // custom layout
 
 	// armor/weapon timers
 	int            item_timer[TIMER_MAX];
