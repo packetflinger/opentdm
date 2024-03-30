073d194c4792a13fd4bfd085d7e749d9456b0bf5
Joe Reid <joe@joereid.com>
2024-03-29 21:15:39 -0400

    Formatting for p_menu.h

--- p_menu.h
+++ p_menu.h
@@ -1,24 +1,20 @@
-
-enum
-{
-	PMENU_ALIGN_LEFT,
-	PMENU_ALIGN_CENTER,
-	PMENU_ALIGN_RIGHT
+enum {
+    PMENU_ALIGN_LEFT,
+    PMENU_ALIGN_CENTER,
+    PMENU_ALIGN_RIGHT
 };
 
-typedef struct pmenuhnd_s
-{
-	struct pmenu_s *entries;
-	int cur;
-	int num;
+typedef struct pmenuhnd_s {
+    struct pmenu_s *entries;
+    int cur;
+    int num;
 } pmenuhnd_t;
 
-typedef struct pmenu_s
-{
-	char	*text;
-	int		align;
-	void	*arg;
-	void	(*SelectFunc)(edict_t *ent, struct pmenu_s *entry);
+typedef struct pmenu_s {
+    char *text;
+    int align;
+    void *arg;
+    void (*SelectFunc)(edict_t *ent, struct pmenu_s *entry);
 } pmenu_t;
 
 void PMenu_Open(edict_t *ent, pmenu_t *entries, int cur, int num);
